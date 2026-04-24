2fe1016d6f361ec17efe3e1184dab16217dac6fa
Gabriel Conde <gabrielcondesilv@gmail.com>
2026-04-24 11:20:14 -0300

    desafio01

---
+++ desafio01.py
@@ -0,0 +1,4 @@
+# Crie um script python que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas de acordo com o valor digitado.
+nome = input("Digite seu nome: ")
+
+print(f'Olá {nome}, seja bem-vindo!')
