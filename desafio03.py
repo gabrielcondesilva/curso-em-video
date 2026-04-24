65fb54530285c94f5697829fd95e124025057206
Gabriel Conde <gabrielcondesilv@gmail.com>
2026-04-24 12:03:14 -0300

    desafio03

---
+++ desafio03.py
@@ -0,0 +1,6 @@
+# Crie um scrip python que leia dois números e tente mostrar a soma entre eles. 
+
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
+
+print(f'A soma entre esses dois números é: {n1} + {n2}')
