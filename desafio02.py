7be43e94403e0766a3970e0e04838db96fa4eebc
Gabriel Conde <gabrielcondesilv@gmail.com>
2026-04-24 11:28:08 -0300

    desafio02

---
+++ desafio02.py
@@ -0,0 +1,7 @@
+# Crie um sript python que leia o dia, o mês e o ano de nascimento de uma pessoa e mostre uma mensagem com a data formatada. 
+
+dia = input("Que dia você nasceu? ")
+mes = input("Que mês você nasceu? ") 
+ano = input("Que ano você nasceu? ")
+
+print(f'Você nasceu em {dia}/{mes}/{ano}')
