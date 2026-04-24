c121d7a6eac4483e440578e09255e037d8d58145
Gabriel Conde <gabrielcondesilv@gmail.com>
2026-04-24 12:07:31 -0300

    Corrigindo o print para o resultado

--- desafio03.py
+++ desafio03.py
@@ -3,4 +3,4 @@
 n1 = int(input('Digite o primeiro número: '))
 n2 = int(input('Digite o segundo número: '))
 
-print(f'A soma entre esses dois números é: {n1} + {n2}')
+print(f'A soma entre esses dois números é:  {n1 + n2}')
