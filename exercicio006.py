8dee85d7cc8c6a4fe837090166811ffb286bd41d
Gabriel Conde <gabrielcondesilv@gmail.com>
2026-04-25 18:28:06 -0300

    Exercício 006

---
+++ exercicio006.py
@@ -0,0 +1,9 @@
+# Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre sua média. 
+nome = input('Olá, qual é o seu nome? ')
+
+nota1 = float(input("Digite sua primeira nota: "))
+
+nota2 = float(input("Digite sua segunda nota: "))
+
+print(f'A média das notas do aluno {nome} é {(nota1 + nota2) / 2}.')
+
