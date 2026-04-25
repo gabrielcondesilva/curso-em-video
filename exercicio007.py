8149d6d5a3baded322880419909c5d2a7d314d33
Gabriel Conde <gabrielcondesilv@gmail.com>
2026-04-25 18:32:02 -0300

    Exercício 007

---
+++ exercicio007.py
@@ -0,0 +1,5 @@
+# Escreva um programa que leia um número em metros e exiba convertido em centimetros e milimetros. 
+
+valor = float(input('Digite um valor em metro: '))
+
+print(f'O valor em metros é {valor}, convertido para centimetros é {valor * 100} e milimetros é {valor * 1000}.')
