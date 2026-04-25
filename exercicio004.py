b1ad4fd01176b9f0a418e5063b54460fceded068
Gabriel Conde <gabrielcondesilv@gmail.com>
2026-04-25 18:17:57 -0300

    Exercício 004

---
+++ exercicio004.py
@@ -0,0 +1,5 @@
+# Faça um programa que leia um número inteiro e mostre seu sucessor e seu antecessor. 
+
+numero = int(input('Digite um número: '))
+
+print(f'O número é {numero}, o seu antecessor é {numero - 1} e seu sucessor é {numero + 1}.')
