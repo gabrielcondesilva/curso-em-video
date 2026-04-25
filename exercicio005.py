d57c506f9e5638d4c386899096c49c68ff85517d
Gabriel Conde <gabrielcondesilv@gmail.com>
2026-04-25 18:22:00 -0300

    Exercício 005

---
+++ exercicio005.py
@@ -0,0 +1,5 @@
+# Crie um algoritimo que leia um número e mostre o seu dobro, triplo e raiz quadrada. 
+
+numero = int(input('Digite um número: '))
+
+print(f'O número é {numero}, o seu dobro é {numero * 2} e seu triplo é {numero * 3}')
